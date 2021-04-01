39b084114fad23725078174d1a807db60d994675
regromrob <romae31@gmail.com>
2021-04-01 14:20:28 -0400

    grey's episodes

---
+++ greys-anatomy/public/js/main.js
@@ -0,0 +1,56 @@
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-thumbs-up')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteSeason)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+async function deleteSeason(){
+    // const sName = this.parentNode.childNodes[1].innerText
+    // const bName = this.parentNode.childNodes[3].innerText
+    const sNum = this.parentNode.childNodes[1].innerText
+    const eNum= this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteSeason', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'seasonNumS': sNum,
+              'episodeNumS': eNum
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function addLike(){
+    const sNum = this.parentNode.childNodes[1].innerText
+    const eNum = this.parentNode.childNodes[3].innerText
+    const tLikes = Number(this.parentNode.childNodes[7].innerText)
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'seasonNumS': sNum,
+              'episodeNumS': eNum,
+              'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
