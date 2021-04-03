93f16fdf5ee78799a1b9a6d6c01cc6d55d0a4313
nicole-gathany <nicole.gathany@gmail.com>
2021-04-03 00:04:36 -0400

    just connected the server and the port. getting read to read or get tomorrow

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 console.log("may the source be with you ")
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function(){
+    console.log('listening on 3000, no relation to andre')
+})
+
+app.get(endpoint, callback)
+
+//i left off at the crudread section on the article leon sent us. i am going to bed because i deserve rest
