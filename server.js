27e8686d61ae273629fe5f8a004b46bf420aa271
nicole-gathany <nicole.gathany@gmail.com>
2021-04-03 15:42:31 -0400

    added node modules,  installed nodemon, and added post requests

--- server.js
+++ server.js
@@ -1,5 +1,3 @@
-console.log("may the source be with you ")
-
 const express = require('express');
 const app = express();
 
@@ -7,11 +5,9 @@ app.listen(3000, function(){
     console.log('listening on 3000, no relation to andre')
 })
 
-// app.get(endpoint, callback) //when i typed in node server, it did not like endpoint. I'm actually not sure I understand that. I think this is just the framework for app.get. will comment out for reference. //this didn't work either so i'm just doing to delete it all but not before committing because i am a hoarder of my own thoughts
-
-//i left off at the crudread section on the article leon sent us. i am going to bed because i deserve rest
 
 app.get('/', (request, response) => { //added es6 syntax to be fancy
     //do something here
-    response.send('Hello World')
-})
+    response.sendFile(__dirname + '/index.html')
+}) //this worked
+
