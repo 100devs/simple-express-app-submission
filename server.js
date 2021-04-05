dbe5687b965b6410009a816ff84cc5a597fdbc26
nicole-gathany <nicole.gathany@gmail.com>
2021-04-04 20:09:21 -0400

    got the post request to work and added request.body to test

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
+//const jsonParser = bodyParser.json()
 
-app.listen(3000, function(){
-    console.log('listening on 3000, no relation to andre')
-})
 
+//app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(bodyParser.json({ type: 'application/*+json' }))
 
 app.get('/', (request, response) => { //added es6 syntax to be fancy
     //do something here
     response.sendFile(__dirname + '/index.html')
 }) //this worked
+
+app.post('/todo', (request, response)=>{
+    console.log('just work please') //post not working right now
+    console.log(request.body)
+})
+
+app.listen(3000, function(){
+    console.log('listening on 3000, no relation to andre')
+})
 
