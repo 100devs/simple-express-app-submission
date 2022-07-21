b7c50ed4d2de95c5a8022e43c045fc83aad4cbfb
Sasha Marshall <sashamarshalldesigns@gmail.com>
2022-07-21 14:59:08 -0700

    autocorrect kills me

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 require('dotenv').config()
 
 const password = process.env.MONGO_PASS
-const PORT = process.eventNames.PORT;
+const PORT = process.env.PORT;
 
 MongoClient.connect(`mongodb+srv://sashamars:${password}@cluster0.0tvvaxm.mongodb.net/?retryWrites=true&w=majority`, {useUnifiedTopology: true})
     .then(client => {
