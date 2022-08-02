3ee6f66020cefb41033f4df9b223fb0c1ac1a891
Narciso Salvador <narcisodsalvador@gmail.com>
2022-08-02 16:23:08 -0700

    added dependencies and gitignore

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
-console.log
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+require('dotenv').config()
+
+const dbConnectionString = process.env.DB_String
+let db,
+    dbName = '',
+    collection
