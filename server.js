17b50d198a039f8fb43aa6aa27bc3ebbd79395fa
Nate Estes <97855282+estesnr@users.noreply.github.com>
2022-08-02 18:32:53 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,67 @@
+// server.js
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+
+MongoClient.connect('mongodb+srv://Mecharossman:Colphia!1@instbase.tmrqqa9.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true })
+    .then(client => {
+        console.log('Connected to Database')
+        const db = client.db('instBase')
+        const instrumentation = db.collection('instrus')
+
+        app.listen(3000, function() {
+            console.log('listening on 3000')
+        })
+        
+        app.use(bodyParser.urlencoded({ extended: true }))
+        app.use(bodyParser.json())
+
+        app.set('views', './views')
+        app.set('view engine', 'ejs')
+        app.get('/', function(req, res) {
+            instrumentation.find().toArray()
+               .then(results => {
+               res.render('index.ejs', { instrumentation: results })
+               })
+            })
+
+        app.get('/style.css', function(req, res) {
+            res.sendFile(__dirname + '/style.css')
+        })
+
+        app.get('/actions.js', function(req, res) {
+            res.sendFile(__dirname + '/actions.js')
+        })
+        
+        app.post('/create', (req, res) => {
+            instrumentation.insertOne(req.body)
+                .then(result => {
+                console.log(result)
+                res.redirect('/')
+                })
+                .catch(error => console.error('error'))
+        })
+
+        app.put('/update', (req, res) => {
+            instrumentation.findOneAndUpdate(
+                { CalSticker : document.querySelector('#checkInCalStick') },
+                {
+                    $set: {
+                        Loc : 'HomeBase',
+                        Date : document.querySelector('#checkInDate')
+                    }
+                },
+                options
+            )
+            .then(result => {
+                console.log(result)
+            })
+            .catch(error => console.error(error))
+
+        })
+        
+    })
+
+
+
