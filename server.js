3a1f43c15c0394a83c536aab9d03de45acf8d82b
Cory Nelson <fullnelsontech@gmail.com>
2022-08-02 19:19:32 -0400

    removed template literal in app.listen

--- server.js
+++ server.js
@@ -83,6 +83,4 @@ app.route("/remove/:id").get((req, res) => {
 })
 
 // Start Server
-app.listen(process.env.PORT || PORT, () =>
-  console.log(`Server is running on port ${PORT}`)
-)
+app.listen(process.env.PORT || PORT, () => console.log(`Server is running!`))
