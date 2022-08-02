5dbae1b53a18bd7e50395ed7c9719ce4a927e8d1
ninjanordbo <101290517+ninjanordbo@users.noreply.github.com>
2022-08-03 00:35:21 +0200

    I completed the challenge: 5 I feel good about my code: 3

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-I completed the challenge: 5
-I feel good about my code: 3
+ORDTAK (which means Proverb in Norwegian)
+
+My mum loves proverbs, and she often answers with proverbs whenever she is asked something. 
+I decided to make an app to store her most used ones and also give her as a birthday present. 
+
+[![hQTYp1.md.jpg](https://iili.io/hQTYp1.md.jpg)](https://freeimage.host/i/hQTYp1)
+
+
+**Check out live project:** https://ordtak.herokuapp.com/
+
+
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, Node.js, Express, EJS, MongoDB.
+
+Building the app in Node and Express was a good choice for this app, since it was a quite small project which did not need too much. 
+
+## Optimizations
+
+I would have loved to add proverb categories and also a search function. I was also thinking of adding upvoting to the proverbs. I would maybe also consider remaking it in React if I was going to remake it. 
+
+## Lessons Learned:
+
+Although this was not a huge app to build, I learned quite a few lessons along the way. This was my first time using EJS and it took me a while to work out how to get it to render the document files properly. It was also a very good learning experience working with MongoDB. Quite a lot of debugging went into getting this app to work with Heroku. I was getting error codes and had to work out why the app was crashing everytime I tried to launch it. 
+
+
--- public/js/main.js
+++ public/js/main.js
@@ -1,93 +1,44 @@
-// Assign a NodeList of all HTMLElements with a class of `fa-trash` to the constant variable `deleteBtn`
-const deleteBtn = document.querySelectorAll('.fa-trash')
-// Assign a NodeList of all HTMLElements that are <span>s and descendants of elements with a class of `item` to the constant variable `item`
-const item = document.querySelectorAll('.item span')
-// Assign a NodeList of all HTMLElements that are <span>s with a class of `completed` and descendants of elements with a class of `item` to the constant variable `itemCompleted`
-const itemCompleted = document.querySelectorAll('.item span.completed')
+const update = document.querySelector('#update-button')
+const next = document.querySelector('#nesteOrdtak')
 
-// Convert `deleteBtn` to an array of HTMLElements, then call `forEach` on it
-Array.from(deleteBtn).forEach((element)=>{
-    // Add an click event listener to each element, calling `deleteItem`
-    element.addEventListener('click', deleteItem)
-})
+update.addEventListener('click', _ => {
+    fetch('/ordtak', {
+      method: 'put',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        
+      })
+    })
+  })
 
-// Convert `item` to an array of HTMLElements, then call `forEach` on it
-Array.from(item).forEach((element)=>{
-    // Add an click event listener to each element, calling `markComplete`
-    element.addEventListener('click', markComplete)
-})
+  next.addEventListener ('click', _ =>{
+    fetch('/', {
+      method:'get',
+      headers:{'Content-Type': 'application/json' },
+      body:JSON.stringify({
+        
+      })
+   
+      
+    })
+  })
 
-// Convert `itemCompleted` to an array of HTMLElements, then call `forEach` on it
-Array.from(itemCompleted).forEach((element)=>{
-    // Add an click event listener to each element, calling `markUnComplete`
-    element.addEventListener('click', markUnComplete)
-})
-
-async function deleteItem(){
-    // From the current <span> (bound to this by the event listener), get the parentNode - the <li> - then get the 1th childNode - the <span>, then retrieve the text content of it by reading the `innerText` property
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        // Make a fetch to the relative path `deleteItem`
-        const response = await fetch('deleteItem', {
-            // Set method of request to DELETE
-            method: 'delete',
-            // Set header `Content-Type` to `application/json` so it knows we're sending JSON, and how to parse our data
-            headers: {'Content-Type': 'application/json'},
-            // Send the object with a property of itemFromJS and value of the `itemText` of the current item as a JSON string
-            body: JSON.stringify({
-              'itemFromJS': itemText
-            })
-          })
-        // Attempt to load and parse the response body as JSON, assigning it to data
-        const data = await response.json()
-        console.log(data)
-        // Reload the webpage
-        location.reload()
-
-    }catch(err){
-        // If there are any errors, console.log them
-        console.log(err)
+  function refreshPage(){
+    window.location.reload();
+    } 
+    
+    function toggleCssMenu(icon) {
+        var cssmenu = document.getElementById('cssmenu');
+        if (icon.className.indexOf('active') == -1) {
+            icon.className = 'menu-icon active';
+            cssmenu.style.display = "block";
+            setTimeout(function(){cssmenu.className = 'active';},0);            
+        }
+        else {
+            icon.className = 'menu-icon';
+            cssmenu.className = '';
+            setTimeout(function(){cssmenu.style.display = "none";},411); 
+        }
     }
-}
 
-async function markComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        // Make a fetch to the relative path `markComplete`
-        const response = await fetch('markComplete', {
-            // Set method of request to PUT
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
 
-    }catch(err){
-        // If any errors are caught, console.log them
-        console.log(err)
-    }
-}
-
-async function markUnComplete(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        // Make a fetch to the relative path `markUnComplete`
-        const response = await fetch('markUnComplete', {
-            method: 'put',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-          })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-
-    }catch(err){
-        console.log(err)
-    }
-}ß
