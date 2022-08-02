005f8eabafc88ecfe22bf6986a1a002adedfa9cf
Ryan <ryanmahara@gmail.com>
2022-08-02 14:12:53 -0500

    add intial route handling

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const app = express();
+
+// will look in the public directory so CSS/JS links don't need "/public" only "/style.css"
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.post('/log', (req, res) => {
+  res.redirect('/');
+});
+
+const PORT = 3000;
+app.listen(PORT, (err) => {
+  console.log(`Server is listening on ${PORT}`);
+});
