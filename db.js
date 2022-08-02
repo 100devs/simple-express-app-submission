298d5ea69b9943353f8739bf92c2dd4a1cdb8f35
Nicholas Parks <101064471+Nick-P-SE@users.noreply.github.com>
2022-08-02 18:57:57 -0400

    Delete db.js

--- db.js
+++
@@ -1,9 +0,0 @@
-const Mongoose = require("mongoose")
-const RemoteDB = 'mongodb+srv://nchlsparks:sGHAyxoZ3rWtlq81@cluster0.knox9.mongodb.net/?retryWrites=true&w=majority'
-const connectDB = async () => {
-    await Mongoose.connect(RemoteDB)
-    .then(client => {
-        console.log('MongoDB connection successful')
-    })
-}
-module.exports = connectDB
