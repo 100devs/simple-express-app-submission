03b60e9e73508bcb78a2d0bf299729262bf733ca
Alanna Clarke <alannacalendar@gmail.com>
2022-08-02 18:35:58 -0600

    adding crud app files

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'songwriter_crud_app'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cors())
+
+
+app.get('/',(request, response)=>{
+    db.collection('songwriters').find().sort({likes: -1}).toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
+
+app.post('/addSong', (request, response) => {
+    db.collection('songwriters').insertOne({songName: request.body.songName,
+    performerName: request.body.performerName, songwriterName: request.body.songwriterName, likes: 0})
+    .then(result => {
+        console.log('Song Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('songwriters').updateOne({songName: request.body.songNameS, performerName: request.body.performerNameS, songwriterName: request.body.songwriterNameS, likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+          }
+    },{
+        sort: {_id: -1},
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteSong', (request, response) => {
+    db.collection('songwriters').deleteOne({songName: request.body.songNameS})
+    .then(result => {
+        console.log('Rapper Deleted')
+        response.json('Rapper Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
