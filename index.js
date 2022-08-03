872b2e71e46879fe35788c18dd6abd7c6a45e05e
Daniel Hyres <danhyres@gmail.com>
2022-08-03 18:00:23 -0400

    deleted uneccesary file

--- index.js
+++
@@ -1,6 +0,0 @@
-//for coding along with the traversy Node JS crash course video on YT
-//this file has nothing to do with BB quotes DB (delete when done)
-
-const url = require('url')
-
-const myUrl = new URL('http://mywebsite.com/hello.html')
