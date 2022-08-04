6cbbc6417094b0179cdda955e5e2110a2e6308e4
Patrick Snowden <patrickbsnowden@outlook.com>
2022-08-03 17:27:42 -0700

    changed server to express

--- moody-songs/server.js
+++ moody-songs/server.js
@@ -1,68 +1,35 @@
-const http = require("http");
-const fs = require("fs");
-const url = require("url");
-const querystring = require("querystring");
-const figlet = require("figlet");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const angrySongs = require("./songs/angrySongs.js");
 const chillSongs = require("./songs/chillSongs.js");
 const happySongs = require("./songs/happySongs.js");
 const sadSongs = require("./songs/sadSongs.js");
 const loveSongs = require("./songs/loveSongs.js");
-
 const PORT = process.env.PORT || 8000;
-const server = http.createServer((req, res) => {
-     const page = url.parse(req.url).pathname;
-     // const params = querystring.parse(url.parse(req.url).query);
-     console.log(page);
 
-     const readWrite = (file, contentType) => {
-          fs.readFile(file, function (err, data) {
-               res.writeHead(200, { "Content-Type": contentType });
-               res.write(data);
-               res.end();
-          });
-     };
+const app = express();
 
-     switch (page) {
-          case "/":
-               readWrite("index.html", "text/html");
-               break;
-          case "/api/angry":
-               res.end(JSON.stringify(angrySongs.angrySongsObj.angrySongs));
-               break;
-          case "/api/happy":
-               res.end(JSON.stringify(happySongs.happySongsObj.happySongs));
-               break;
-          case "/api/sad":
-               res.end(JSON.stringify(sadSongs.sadSongsObj.sadSongs));
-               break;
-          case "/api/inLove":
-               res.end(JSON.stringify(loveSongs.loveSongsObj.loveSongs));
-               break;
-          case "/api/chill":
-               res.end(JSON.stringify(chillSongs.chillSongsObj.chillSongs));
-               break;
-          case "/css/styles.css":
-               fs.readFile("css/styles.css", function (err, data) {
-                    res.write(data);
-                    res.end();
-               });
-               break;
-          case "/js/main.js":
-               readWrite("js/main.js", "text/javascript");
-               break;
-          default:
-               figlet("404!!", function (err, data) {
-                    if (err) {
-                         console.log("Something went wrong...");
-                         console.dir(err);
-                         return;
-                    }
-                    res.write(data);
-                    res.end();
-               });
-               break;
-     }
+app.use(express.static("public"));
+app.use(cors());
+
+app.get("/", (req, res) => {
+     res.sendFile(path.join(__dirname + "/index.html"));
+});
+app.get("/api/angry", (req, res) => {
+     res.json(angrySongs.angrySongsObj.angrySongs);
+});
+app.get("/api/happy", (req, res) => {
+     res.json(happySongs.happySongsObj.happySongs);
+});
+app.get("/api/sad", (req, res) => {
+     res.json(sadSongs.sadSongsObj.sadSongs);
+});
+app.get("/api/inLove", (req, res) => {
+     res.json(loveSongs.loveSongsObj.loveSongs);
+});
+app.get("/api/chill", (req, res) => {
+     res.json(chillSongs.chillSongsObj.chillSongs);
 });
 
-server.listen(PORT);
+app.listen(PORT, () => console.log("server running"));
