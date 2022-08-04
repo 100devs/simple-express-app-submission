867d8b62bfac86166a72dc913e50742bd7473d86
1llionaires <paulchodev@gmail.com>
2022-08-04 14:43:33 -0700

    simple crud app submission

---
+++ public/main.js
@@ -0,0 +1,40 @@
+const update = document.querySelector('#update-button')
+const deleteButton = document.querySelector('#delete-button')
+const messageDiv = document.querySelector('#message')
+
+update.addEventListener('click', _ =>{
+    fetch('/quotes',{
+        method: 'put',
+        headers: {'Content-Type':'application/json'},
+        body: JSON.stringify({
+            name: 'Michael',
+            quote: 'I… Declare…. Bankruptcy!'
+        })
+    })
+    .then(res=>{
+        if(res.ok)return res.json()
+    })
+    .then(response =>{
+        window.location.reload(true)
+    })
+})
+
+deleteButton.addEventListener('click', _ =>{
+    fetch('/quotes', {
+        method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Michael'
+    })
+  })
+    .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(response => {
+    if (response === 'No quote to delete') {
+        messageDiv.textContent = 'No more Michael quotes to delete'
+      } else {
+      window.location.reload(true)
+      }
+    })
+})
