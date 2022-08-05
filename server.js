dad4fafbb6ab8e5fc9687a558e447e4b78d73122
callmematt <mconnolly@fastmail.com>
2022-08-04 20:23:13 -0400

    initial commit

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const MongoClient = require('mongodb').MongoClient;
 const PORT = 2121;
 require('dotenv').config();
 
+// middleware
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // database instance
 let db,
     dbConnectionStr = process.env.DB_STRING,
@@ -15,25 +21,25 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
         console.log(`Connected to ${dbName} database.`);
         db = client.db(dbName);
         const booksCollection = db.collection('books');
+
+        app.get('/', async (req, res) => {
+            // get documents from MongoDB
+            const bookItems = await booksCollection.find().toArray();
+        
+            // render root/home view
+            res.render('index.ejs', { books: bookItems });
+        })
+
+        app.post('/addBook', (req, res) => {
+            // add book to bookshelf
+            booksCollection.insertOne(req.body)
+            .then(result => {
+                res.redirect('/');
+            })
+            .catch(error => console.log(error));
+        })
     })
     .catch(error => console.log(error));
-
-app.set('view engine', 'ejs');
-app.use(express.static('public'));
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.get('/', async (req, res) => {
-    // get documents from MongoDB
-
-    // render root view
-    res.render('index.ejs')
-})
-
-app.post('/addBook', (req, res) => {
-    booksCollection.insertOne
-})
-
 
 
 
