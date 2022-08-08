aa56cbc0e0cfce5f0dec806f8f1d012a85115822
JP <johnpaulmitra@yahoo.com>
2022-08-07 23:40:46 -0500

    Connected Google Client ID & Client Secret

---
+++ config/passport.js
@@ -0,0 +1,39 @@
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const User = require('../models/User')
+
+const googleLogin = (passport) => {
+  passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+    const newUser = {
+      googleId: profile.id,
+      displayName: profile.displayName,
+      firstName: profile.name.givenName,
+      lastName: profile.name.familyName,
+      image: profile.photos[0].value
+    }
+    try {
+      let user = await User.findOne({ googleId: profile.id })
+      if (user) {
+        done(null, user)
+      } else {
+        user = await User.create(newUser)
+        done(null, user)
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }));
+  passport.serializeUser( (user, done) => {
+    done(null, user.id)
+  })
+  passport.deserializeUser( (id, done) => {
+    User.findById(id, (err, user) => done(err, user))
+  })
+}
+
+module.exports = googleLogin
