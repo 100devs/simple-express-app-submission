84e30dde4ff9cfc8aa4b4758c9a3858e72021920
JP <johnpaulmitra@yahoo.com>
2022-08-07 23:38:11 -0500

    Connected DB to app

---
+++ config/db.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose')
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
+    console.log(`MongoDB Connected: ${conn.connection.host}`)
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+module.exports = connectDB
