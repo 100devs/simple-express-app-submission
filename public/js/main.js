f10e47f68c53d425f2dd9c6817a07955403305f5
matt-conn <mconnolly@fastmail.com>
2022-08-08 21:23:17 -0400

    complete add, mark as read for app

---
+++ public/js/main.js
@@ -0,0 +1,20 @@
+// const deleteBtn = document.querySelectorAll('.fa-trash');
+
+
+
+
+// Array.from(deleteBtn).forEach(el => {
+//     el.addEventListener('click', deleteBook);
+// })
+
+
+
+
+
+
+
+
+// async function deleteBook() {
+//     const book = this.parentNode.parentNode.childNodes[1].innerText;
+//     console.log(book)
+// }
--- server.js
+++ server.js
@@ -1,4 +1,8 @@
+// THANK YOU MAYANWOLFE! 
+// https://www.youtube.com/watch?v=s5FtvtYgc68&t=1350s
+
 const express = require('express');
+const { ObjectId } = require('mongodb');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
 const PORT = 2121;
@@ -15,38 +19,61 @@ let db,
     dbConnectionStr = process.env.DB_STRING,
     dbName = 'book'
 
-// connect to database
+// connect to database, create paths
 MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     .then(client => {
         console.log(`Connected to ${dbName} database.`);
         db = client.db(dbName);
         const booksCollection = db.collection('books');
 
+        // RENDER BOOKSHELF
         app.get('/', async (req, res) => {
-            try{
-                // get documents from MongoDB
-                const bookItems = await booksCollection.find().toArray();
-        
-                // render root/home view
-                res.render('index.ejs', { books: bookItems });
-            } catch (error) {
-                if (error) return res.status(500).send(erro);
-            }
+            // get documents (books) from MongoDB
+            const bookItems = await booksCollection.find().toArray();
+    
+            // render root view, send documents (book to ejs template)
+            res.render('index.ejs', { books: bookItems });
         })
 
+        // ADD BOOK
         app.post('/addBook', (req, res) => {
             // add book to bookshelf
-            booksCollection.insertOne(req.body)
+            booksCollection.insertOne({title: req.body.title, author: req.body.author, read: false})
             .then(result => {
+                console.log('Book added.')
                 res.redirect('/');
             })
             .catch(error => console.log(error));
         })
+
+        // MARK READ/UNREAD
+        app
+            .route("/readBook/:id")
+            .get((req, res) => {
+                const id = req.params.id;
+                booksCollection.updateOne(
+                    { _id:ObjectId(id) },
+                    { $set: { "read" : true } }    
+                );
+                
+                console.log('Book read.');
+                res.redirect('/');
+            })
+        
+            // DELETE BOOK
+        app
+            .route("/deleteBook/:id")
+            .get((req, res) => {
+                const id = req.params.id;
+                booksCollection.deleteOne({_id:ObjectId(id)});
+                
+                console.log('Book deleted.');
+                res.redirect('/');
+            })
+
     })
     .catch(error => console.log(error));
 
-
-
-app.listen(PORT, function() {
+app.listen(process.env.PORT || PORT, () => {
     console.log(`server is running on PORT ${PORT}`);
 })
