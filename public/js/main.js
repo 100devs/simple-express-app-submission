3f0e1c907e00c62d37f5efabaa3cd5a792429200
greenWindBreaker <102470998+greenWindBreaker@users.noreply.github.com>
2022-08-12 15:46:29 -0400

    replaced function changBG to function colorChange that actuallys changes the background color of the home page when user clicks on the span text

--- public/js/main.js
+++ public/js/main.js
@@ -6,7 +6,7 @@ Array.from(deleteBtn).forEach((element)=>{
     element.addEventListener('click', deleteColor)
 })
 Array.from(item).forEach((element)=>{
-    element.addEventListener('click',changeBG)
+    element.addEventListener('click',colorChange)
 })
 
 async function deleteColor(){
@@ -27,22 +27,27 @@ async function deleteColor(){
     }
 }
 
-async function changeBG(){
-    const itemText = this.parentNode.childNodes[1].innerText
-    try{
-        const response = await fetch('changeBG',{
-            method: 'put',
-            headers: {'Content-Type':'application/json'},
-            body: JSON.stringify({
-                'itemFromJS': itemText
-            })
-        })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
-    }catch(err){
-        console.log(err)
-    }
+// async function changeBG(){
+//     const itemText = this.parentNode.childNodes[1].innerText
+//     try{
+//         const response = await fetch('changeBG',{
+//             method: 'put',
+//             headers: {'Content-Type':'application/json'},
+//             body: JSON.stringify({
+//                 'itemFromJS': itemText
+//             })
+//         })
+//         const data = await response.json()
+//         console.log(data)
+//         location.reload()
+//     }catch(err){
+//         console.log(err)
+//     }
+// }
+
+function colorChange(){
+    const itemText = this.parentNode.childNodes[1].innerText;
+    document.body.style.backgroundColor = itemText
 }
 
 console.log('📶📶📶')//connected to index.ejs, successfull
