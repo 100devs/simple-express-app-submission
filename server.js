df56fd5c19143f219ff66468e6aa2fafa514639a
Carlos Perez <carlosperez@Carloss-MacBook-Pro.local>
2022-08-15 14:43:38 -0500

    added some styles

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const path = require('path')
 const cors = require('cors')
 require('dotenv').config()
 const PORT = 3000
@@ -10,7 +11,7 @@ const PORT = 3000
 //Middlewares
 
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 app.use(cors())
