fbb67e2d15a586d6932d4135e07a9583dfc89f5e
matt-conn <mconnolly@fastmail.com>
2022-08-15 11:50:39 -0400

    changes

--- server.js
+++ server.js
@@ -11,7 +11,6 @@ require('dotenv').config();
 // middleware
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
