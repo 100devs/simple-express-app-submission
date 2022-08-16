4f91e9fd61c53328e6ba7908c0eb96a99aa0656f
Carlos Perez <carlosperez@Carloss-MacBook-Pro.local>
2022-08-16 12:06:07 -0500

    finished minimum  viable product

--- models/Note.js
+++ models/Note.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const noteSchema = new Schema({
+const noteSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true
--- server.js
+++ server.js
@@ -6,11 +6,14 @@ const mongoose = require('mongoose')
 const path = require('path')
 const cors = require('cors')
 require('dotenv').config()
+const methodOverride = require('method-override')
 const PORT = 3000
+const Note = require('./models/Note')
 
 //Middlewares
 
 app.set('view engine', 'ejs')
+app.use(methodOverride('_method'))
 app.use(express.static(__dirname + '/public'))
 app.use(express.urlencoded({extended: true}))
 app.use(express.json())
@@ -25,14 +28,66 @@ mongoose.connect(process.env.DB_CONNECTION_STRING, {}, () => {
 
 //Routes
 
+//Get all notes
 app.get("/", async (req, res) => {
     try {
-        res.render('index.ejs')
+        const notes = await Note.find({}).lean()
+        res.render('index.ejs', { notes })
     } catch (error) {
         console.log(error)
     }
 })
 
+//Go to the new note page
+app.get("/add", (req, res) => {
+    try {
+        res.render('newNote.ejs')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//Go to the edit page
+app.get('/edit/:id', async (req, res) => {
+    try {
+        const note = await Note.findOne({_id: req.params.id})
+        res.render('edit.ejs', { note })
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//Make new note
+app.post('/add', async (req, res) => {
+    try {
+        const newNote = await Note.create({title: req.body.title, content: req.body.content})
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//Edit the note in the database
+app.put('/edit/:id', async (req, res) => {
+    try {
+        await Note.findByIdAndUpdate(req.params.id, {title: req.body.title, content: req.body.content})
+        res.redirect('/')
+    } catch (error) {
+        console.log(error)
+    }
+})
+
+//Delete a note
+app.delete('/deleteNote/:id', async (req, res) => {
+    try {
+        await Note.findOneAndDelete({_id: req.params.id}).then(result => {
+            console.log('entry deleted')
+            res.json('Note deleted')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+})
 
 
 
