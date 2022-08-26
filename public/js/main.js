75f5d92ff8bf77120f6124bef574d23ac5de3a82
Edgar Herrera <edgarh09lfc@gmail.com>
2022-08-25 22:32:07 -0700

    I added my completed code and files

---
+++ public/js/main.js
@@ -0,0 +1,53 @@
+const deleteText = document.querySelectorAll('.fa-trash')
+const thumbText = document.querySelectorAll('.fa-plus')
+
+Array.from(deleteText).forEach((element)=>{
+    element.addEventListener('click', deleteItem)
+})
+
+Array.from(thumbText).forEach((element)=>{
+    element.addEventListener('click', addLike)
+})
+
+async function deleteItem(){
+    const sName = this.parentNode.childNodes[1].innerText
+    const bName = this.parentNode.childNodes[3].innerText
+    try{
+        const response = await fetch('deleteItem', {
+            method: 'delete',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'stageNameS': sName,
+              'birthNameS': bName
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+async function addLike(){
+    const sName = this.parentNode.childNodes[1].innerText
+    console.log(sName)
+    const tLikes = Number(this.parentNode.childNodes[3].innerText)
+    try{
+        const response = await fetch('addOneLike', {
+            method: 'put',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+              'stageNameS': sName,
+              'likesS': tLikes
+            })
+          })
+        const data = await response.json()
+        console.log(data)
+        location.reload()
+
+    }catch(err){
+        console.log(err)
+    }
+}
---
+++ server.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const PORT = 2121
+require('dotenv').config()
+
+
+let db,
+    dbConnectionStr = process.env.DB_STRING,
+    dbName = 'grocery'
+
+MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+    })
+    
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.get('/',(request, response)=>{
+    db.collection('items').find().toArray()
+    .then(data => {
+        response.render('index.ejs', { info: data })
+    })
+    .catch(error => console.error(error))
+})
+
+app.post('/addItem', (request, response) => {
+    db.collection('items').insertOne({stageName: request.body.stageName, likes: 1})
+    .then(result => {
+        console.log('Item Added')
+        response.redirect('/')
+    })
+    .catch(error => console.error(error))
+})
+
+app.put('/addOneLike', (request, response) => {
+    db.collection('items').updateOne({stageName: request.body.stageNameS, /*birthName: request.body.birthNameS, */ likes: request.body.likesS},{
+        $set: {
+            likes:request.body.likesS + 1
+          }
+    },{
+        upsert: true
+    })
+    .then(result => {
+        console.log('Added One Like')
+        response.json('Like Added')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.delete('/deleteItem', (request, response) => {
+    db.collection('items').deleteOne({stageName: request.body.stageNameS})
+    .then(result => {
+        console.log('Item Deleted')
+        response.json('Item Deleted')
+    })
+    .catch(error => console.error(error))
+
+})
+
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`Server running on port ${PORT}`)
+})
