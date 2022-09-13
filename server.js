520e1b868f16d822ee946287583a9b227b700201
Andy Wood <85075266+TheWoodenMan@users.noreply.github.com>
2022-09-13 01:23:12 +0100

    add/delete functionality
    - added ability to create new records
    - added ability to delete records
    - log entries now correctly sort by date descending.

--- server.js
+++ server.js
@@ -35,10 +35,48 @@ app.use(express.json());
 
 app.get("/", async (request, response) => {
 	// This mongodb search returns all "entries" entries
-	const entries = await db.collection("entries").find().toArray();
+	const entries = await db
+		.collection("entries")
+		.find()
+		.sort({ "date": -1 })
+		.toArray();
+	console.log(entries);
 
 	// This render response sends the now modified ejs to the user
 	response.render("index.ejs", { entries: entries });
+});
+
+// This express router creates new log entries from the form
+app.post("/addEntry", (req, res) => {
+	// create a new document in the collection "entries"
+	db.collection("entries")
+		.insertOne({
+			summary: req.body.summary,
+			date: req.body.date,
+			cause: req.body.cause,
+			action: req.body.action,
+			result: req.body.result,
+			learned: req.body.learned,
+		})
+		.then((result) => {
+			console.log(`Log entry ${result.insertedId} added`);
+			console.log("You have updated your journal.");
+			res.redirect("/");
+		})
+		.catch((err) => {
+			console.log(err);
+		});
+});
+
+app.delete("/deleteEntry", (req, res) => {
+	db.collection("entries")
+		.deleteOne({ summary: req.body.entryFromJS })
+		.then((result) => {
+			console.log(result);
+			console.log("Log Entry Deleted");
+			res.json("Log Entry Deleted");
+		})
+		.catch((err) => console.error(err));
 });
 
 app.listen(process.env.PORT || PORT, () => {
