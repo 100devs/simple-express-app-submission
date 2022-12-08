7643590386ef349335935e494161199f1232170f
Hattie <82660400+HattieTavares@users.noreply.github.com>
2022-12-07 22:09:35 -0500

    server file update

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ require("dotenv").config({ path: "./config/.env"})
 require("./config/passport")(passport)
 
 connectDB().then(() => {
-    app.listen(process.env.PORT || PORT, ()=> console.log(`Server is running on port ${PORT}`))
+    app.listen(process.env.PORT || PORT, ()=> {
+        console.log(`Server is running on port ${PORT}`)
+    })
 })
 
 // Set Middleware
