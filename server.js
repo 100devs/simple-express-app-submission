0c4b0cb3197423330b08f442349fc3dd1ebb319a
Neil McGuire <12nmcguire@gmail.com>
2023-04-24 13:28:28 +0700

    hello world

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
+const express = require("express")
 
 const myApplication = express()
 
 const portNumber = 5000
 myApplication.listen(portNumber, function () {
-    console.log(`listening on ${portNumber}`)
+  console.log(`listening on ${portNumber}`)
 })
 
+// myApplication.get(endpoint, callback)
+myApplication.get("/", (request, response) => {
+  response.send("Hello World")
+})
 
 console.log("May the force be with you")
