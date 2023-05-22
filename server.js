0f8617801d53c62198ecbe7b0f786157dc89e054
Neil McGuire <12nmcguire@gmail.com>
2023-05-22 07:44:20 +0700

    images instead of text

--- server.js
+++ server.js
@@ -8,22 +8,22 @@ const PORT = process.env.PORT || 3000
 const myApplication = express()
 
 MongoClient.connect(
-  "mongodb+srv://12nmcguire:a0uC7h7gXC3I9svt@cluster0.jeadbpg.mongodb.net/text?retryWrites=true&w=majority",
+  "mongodb+srv://12nmcguire:a0uC7h7gXC3I9svt@cluster0.jeadbpg.mongodb.net/images?retryWrites=true&w=majority",
   { useUnifiedTopology: true }
 )
   .then((client) => {
-    console.log(`Connected to database`)
-    const db = client.db("crud-text")
-    const text = db.collection("text")
+    console.log(`Connected to database`);
+    const db = client.db("crud-text");
+    const images = db.collection("images");
 
-    myApplication.use(bodyParser.urlencoded({ extended: true }))
-    myApplication.use(express.static("public"))
-    myApplication.use(bodyParser.json())
-    myApplication.set("view engine", "ejs")
+    myApplication.use(bodyParser.urlencoded({ extended: true }));
+    myApplication.use(express.static("public"));
+    myApplication.use(bodyParser.json());
+    myApplication.set("view engine", "ejs");
 
     myApplication.listen(PORT, function () {
-      console.log(`listening on ${PORT}`)
-    })
+      console.log(`listening on ${PORT}`);
+    });
 
     // myApplication.get(endpoint, callback)
     myApplication.get("/", (request, response) => {
@@ -32,53 +32,53 @@ MongoClient.connect(
 
       // read text
       const cursor = db
-        .collection("text")
+        .collection("images")
         .find()
         .toArray()
         .then((results) => {
-          response.render("index.ejs", { textArray: results })
+          response.render("index.ejs", { textArray: results });
         })
-        .catch((err) => console.error(err))
-    })
+        .catch((err) => console.error(err));
+    });
 
-    myApplication.post("/text", (request, response) => {
-      console.log(request.body)
-      text
+    myApplication.post("/images", (request, response) => {
+      console.log(request.body);
+      images
         .insertOne(request.body)
         .then((result) => {
-          console.log("redirecting")
-          response.redirect("/")
+          console.log("redirecting");
+          response.redirect("/");
         })
-        .catch((err) => console.error(err))
-    })
+        .catch((err) => console.error(err));
+    });
 
-    myApplication.put("/text", (request, response) => {
-      text
+    myApplication.put("/images", (request, response) => {
+      images
         .findOneAndUpdate(
           {},
           {
             $set: {
-              text: request.body.text,
+              images: request.body.image,
             },
           },
           {
             upsert: true,
           }
         )
-        .then((result) => response.json("successfully updated text"))
-        .catch((err) => console.error(err))
-    })
+        .then((result) => response.json("successfully updated image"))
+        .catch((err) => console.error(err));
+    });
 
-    myApplication.delete("/text", (request, response) => {
-      text
+    myApplication.delete("/images", (request, response) => {
+      images
         .deleteOne()
         .then((result) => {
-          response.json("Deleted quote")
+          response.json("Deleted quote");
         })
-        .catch((err) => console.error(err))
-    })
+        .catch((err) => console.error(err));
+    });
   })
-  .catch((err) => console.error(err))
+  .catch((err) => console.error(err));
 
 // async function connectMongoClient(MongoClient) {
 //   try {
