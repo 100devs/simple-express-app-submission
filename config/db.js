14d3b1ec5212e6d8b06eabc724041a0439e7830e
Neil McGuire <12nmcguire@gmail.com>
2023-05-22 14:12:46 +0700

    prep merge

--- config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  url: "mongodb+srv://12nmcguire:a0uC7h7gXC3I9svt@cluster0.jeadbpg.mongodb.net/images?retryWrites=true&w=majority",
-  database: "crud-text",
-  imgBucket: "photos",
-};
--- middleware/upload.js
+++
@@ -1,4 +0,0 @@
-const util = require("util");
-const multer = require("multer");
-const { GridFsStorage } = require("multer-gridfs-storage");
-const dbConfig = require("../config/db.js");
